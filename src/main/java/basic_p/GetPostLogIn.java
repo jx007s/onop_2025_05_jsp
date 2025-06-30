78fde3031226be4cfdedb2f657c653bfe4fb8dd2
jx007s <jx007s@gmail.com>
2025-06-30 10:37:45 +0900

    6_30_1

--- src/main/java/basic_p/GetPostLogIn.java
+++ src/main/java/basic_p/GetPostLogIn.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Servlet implementation class GetPostLogIn
@@ -42,8 +43,36 @@ public class GetPostLogIn extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 회원정보
+		HashMap<String, MemberDTO> mems = new HashMap<String, MemberDTO>();
+		mems.put("aaa",new MemberDTO("aaa","1111","지예금"));
+		mems.put("bbb",new MemberDTO("bbb","2222","지예은"));
+		mems.put("ccc",new MemberDTO("ccc","3333","지예동"));
+		mems.put("ddd",new MemberDTO("ddd","4444","지예철"));
+		mems.put("eee",new MemberDTO("eee","5555","지예납"));
+		
+		String res = "로그인실패";
+		
+		String pid = request.getParameter("pid");
+		String pw = request.getParameter("pw");
+		//System.out.println(pid+","+pw);
+		
+		if(mems.containsKey(pid)) {
+			MemberDTO dto =  mems.get(pid);
+			if(dto.pw.equals(pw)) {
+				//System.out.println(pid+","+pw+" 일치");
+				res = dto.pname+"님 안녕하세요";
+			}
+		}
+		
+		
+		
+		
+		
+		
 		response.setContentType("text/html;charset=UTF-8");
-		response.getWriter().println("난 post");
+		response.getWriter().println(res);
 	}
 
 }
---
+++ src/main/java/basic_p/MemberDTO.java
@@ -0,0 +1,15 @@
+package basic_p;
+
+public class MemberDTO {
+
+	String pid, pw, pname;
+
+	MemberDTO(String pid, String pw, String pname) {
+		
+		this.pid = pid;
+		this.pw = pw;
+		this.pname = pname;
+	}
+	
+	
+}
