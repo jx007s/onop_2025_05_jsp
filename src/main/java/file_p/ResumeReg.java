fce097f3042d469855216128df67d20d24100e17
jx007s <jx007s@gmail.com>
2025-07-04 14:28:44 +0900

    7_4_3

--- src/main/java/file_p/ResumeReg.java
+++ src/main/java/file_p/ResumeReg.java
@@ -6,8 +6,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.regex.Pattern;
 
 /**
  * Servlet implementation class ResumeReg
@@ -17,9 +20,7 @@ import java.net.URLEncoder;
 public class ResumeReg extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public ResumeReg() {
         super();
         // TODO Auto-generated constructor stub
@@ -33,13 +34,49 @@ public class ResumeReg extends HttpServlet {
 		String birth=request.getParameter("birth");
 		String addr=request.getParameter("addr");
 		
-		System.out.println(pname+birth+addr);
+		//System.out.println(pname+birth+addr);
 		
-		String query = "?pname="+URLEncoder.encode(pname,"UTF-8");
-		query += "&birth="+URLEncoder.encode(birth,"UTF-8");
-		query += "&addr="+URLEncoder.encode(addr,"UTF-8");
+		String goUrl = "file/resumeForm.jsp";
+		//1개 가져오기
+		Part pic = request.getPart("pic");
+		String fileName = pic.getSubmittedFileName();
+		int dotPos = fileName.lastIndexOf(".");
+		String ext = fileName.substring(dotPos+1);
 		
-		response.sendRedirect("file/resumeView.jsp"+query);
+		//이미지 파일일 경우에만 저장 및 보기
+		if(Pattern.matches("gif|png|jpeg|jpg|bmp", ext.toLowerCase())) {
+			
+			System.out.println(pic.getName()+","+pic.getContentType()+","+pic.getSize()+","+fileName);
+			
+			String dirPath = request.getServletContext().getRealPath("dsttt");
+			dirPath = "D:\\public\\onop\\onop2025_05\\public\\study\\java_work\\jspPrj\\src\\main\\webapp\\dsttt";
+			
+			fileSave(dirPath, pic);
+			
+			
+			String query = "?pname="+URLEncoder.encode(pname,"UTF-8");
+			query += "&birth="+URLEncoder.encode(birth,"UTF-8");
+			query += "&addr="+URLEncoder.encode(addr,"UTF-8");
+			query += "&pic="+URLEncoder.encode(pic.getSubmittedFileName(),"UTF-8");
+			
+			goUrl = "file/resumeView.jsp"+query;
+		}
+		
+		
+		
+		
+		
+		response.sendRedirect(goUrl);
+	}
+	
+	void fileSave(String dirPath, Part pp) {
+		try {
+			pp.write(dirPath+"\\"+pp.getSubmittedFileName());
+			pp.delete();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/webapp/file/resumeView.jsp
+++ src/main/webapp/file/resumeView.jsp
@@ -5,6 +5,11 @@
 <head>
 <meta charset="UTF-8">
 <title>이력서보기</title>
+<style>
+img{
+	width:200px;
+}
+</style>
 </head>
 <body>
 <h1>이력서보기</h1>
@@ -12,7 +17,9 @@
 	<tr><td>이름</td><td><%=request.getParameter("pname") %></td></tr>
 	<tr><td>생년월일</td><td><%=request.getParameter("birth") %></td></tr>
 	<tr><td>주소</td><td><%=request.getParameter("addr") %></td></tr>
-	<tr><td>사진</td><td><%=request.getParameter("pic") %></td></tr>
+	<tr><td>사진</td><td>
+	<img alt="" src="../dsttt/<%=request.getParameter("pic") %>">
+	</td></tr>
 	
 </table>
 </body>
