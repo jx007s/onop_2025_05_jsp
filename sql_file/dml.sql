d505cc5ade5beb45e293d3c3756bd0b74edea8f8
jx007s <jx007s@gmail.com>
2025-07-07 15:11:41 +0900

    7_7_3

--- sql_file/dml.sql
+++ sql_file/dml.sql
@@ -39,4 +39,28 @@ select id, name, height , height + 20 as hh20 from mem2;
 select id, name, height , height + 20 as hh20 from mem2
 where height > 180;
 
+/*
+
+EXAM 테이블을 생성 및 데이터 입력하세요
+
+컬럼명
+
+시번,  학기,   종류,   응시생, 응시일,  국어, 영어,  수학
+id,  hakgi, name,  pid, reg_date, kor, eng,  mat
+숫자  숫자,   문자     문자   날짜     숫자   숫자  숫자
+
+1,1 , semi ddd '2019-5-4' 77 78 71
+2,1 , semi eee '2019-5-4' 87 88 81
+3,1 , final ddd '2019-7-4' 56 65 71
+4,1 , final fff '2019-7-4' 76 65 41
+5,2 , semi fff '2019-10-4' 97 98 91
+6,2 , semi mmm '2019-10-4' 67 68 61
+7,2 , final rrr '2019-12-4' 76 85 91
+8,2 , final ttt '2019-12-4' 66 85 21
+*/
+
+
+
+
+
 
